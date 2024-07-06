e09ff64f6a1e74f98e47b8fe4c6b29caa68c15e8
mukaramarshad131 <mukaramarshad786@gmail.com>
2024-07-06 21:55:06 +0500

    Individual dashboard
    worked on individual dashboard setting , added two components in blank component

--- src/pages/sys/others/blank.tsx
+++ src/pages/sys/others/blank.tsx
@@ -1,5 +1,13 @@
 import Card from '@/components/card';
 
+import IndividualBannerCard from './IndividaulBannerCard';
+import { TrialLinkCard } from './trialLinkCard';
+
 export default function BlankPage() {
-  return <Card />;
+  return (
+    <Card className="flex flex-row gap-2 max-lg:flex-col">
+      <IndividualBannerCard />
+      <TrialLinkCard />
+    </Card>
+  );
 }
---
+++ src/pages/sys/others/trialLinkCard.tsx
@@ -0,0 +1,40 @@
+import { useUserInfo } from '@/store/userStore';
+
+export function TrialLinkCard() {
+  const { username } = useUserInfo();
+  return (
+    <div
+      className=" flex  flex-col items-center justify-between rounded-2xl p-6 py-12 max-lg:w-full"
+      style={{ background: '#92d6e5', color: 'white' }}
+    >
+      <h1 className="text-'#08979C' text-xl font-bold">Free Individual Assessment </h1>
+      <div
+        style={{ color: 'white', fontSize: '15px' }}
+        className="mx-auto mb-6 mt-4 max-w-sm  text-sm  md:mx-0"
+      >
+        Join our EQ360 Emotional Intelligence Assessment free trial!. Track your progress with
+        MyEQ360 and master your emotional skills for greater success. Understand yourself better and
+        enhance your communication abilities with our comprehensive evaluation. Start mastering your
+        emotions today and unlock your full potential!
+      </div>
+      <div className="flex flex-col gap-5">
+        <a
+          href="https://myeq360.com/"
+          target="_blank"
+          className=" text-base font-normal"
+          style={{ color: 'white' }}
+          rel="noreferrer"
+        >
+          What is your Emotional Intelligence?
+        </a>
+        <button
+          className="font-mediumtext-black flex items-center justify-center rounded-lg px-2 py-1 shadow-none md:m-0"
+          style={{ backgroundColor: '#0092B3', color: '#fff' }}
+          onClick={() => window.open('https://myeq360.com/')}
+        >
+          <span className="ml-2 font-normal">Attempt Free Assessment</span>
+        </button>
+      </div>
+    </div>
+  );
+}
