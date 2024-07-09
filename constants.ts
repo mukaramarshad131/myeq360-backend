647710e957b6f6decdded4cff17760894cc5d7db
mukaramarshad131 <mukaramarshad786@gmail.com>
2024-07-09 16:06:19 +0500

    added constants

---
+++ constants.ts
@@ -0,0 +1,35 @@
+import glass_bag from '@/assets/images/glass/ic_glass_bag.png';
+import glass_buy from '@/assets/images/glass/ic_glass_buy.png';
+import glass_message from '@/assets/images/glass/ic_glass_message.png';
+import glass_users from '@/assets/images/glass/ic_glass_users.png';
+
+const IndStatsCards = [
+  {
+    id: 1,
+    cover: { glass_bag },
+    title: 'Understanding Emotions',
+    des: ' Gain insight into emotions for better self-awareness and decision-making.',
+    bg: '',
+  },
+  {
+    id: 2,
+    cover: { glass_users },
+    title: 'Personal Growth',
+    des: ' Develop resilience and adaptability to thrive in various life situations.',
+    bg: '',
+  },
+  {
+    id: 3,
+    cover: { glass_buy },
+    title: 'Effective Communication',
+    des: ' Enhance interpersonal skills for clearer and more meaningful interactions.',
+    bg: '',
+  },
+  {
+    id: 4,
+    cover: { glass_message },
+    title: 'Positive Relationships:',
+    des: ' Promote supportive environments and teamwork for greater success.',
+    bg: '',
+  },
+];
