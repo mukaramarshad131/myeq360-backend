82f4425332e452dfd76a5c4d7ea33ce42aa7e216
mukaramarshad131 <mukaramarshad786@gmail.com>
2024-07-09 10:39:45 +0500

    conflicts resolve

--- src/pages/sys/others/blank.tsx
+++ src/pages/sys/others/blank.tsx
@@ -2,11 +2,11 @@ import Card from '@/components/card';
 
 import Faq from './Faq';
 import IndividualBannerCard from './IndividaulBannerCard';
-import { TrialLinkCard } from './trialLinkCard';
+import { TrialLinkCard } from './TrialLinkCard';
 
 export default function BlankPage() {
   return (
-    <Card className="flex flex-col items-start justify-start">
+    <Card className="flex flex-col">
       <div className="flex flex-row gap-2 max-lg:flex-col">
         <IndividualBannerCard />
         <TrialLinkCard />
