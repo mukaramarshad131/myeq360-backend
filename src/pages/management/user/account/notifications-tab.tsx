5421640c4811e2afba9deb9fca4f57bfa8c0472e
mukaramarshad131 <161040018+mukaramarshad131@users.noreply.github.com>
2024-07-10 16:18:22 +0500

    minor change

--- src/pages/management/user/account/notifications-tab.tsx
+++ src/pages/management/user/account/notifications-tab.tsx
@@ -1,7 +1,6 @@
 import { App, Button, Col, Row, Switch, Typography } from 'antd';
 
 import Card from '@/components/card';
-import SkillAssessmentResult from '@/components/skill-assessment-result';
 import { useThemeToken } from '@/theme/hooks';
 
 export default function NotificationsTab() {
@@ -14,69 +13,66 @@ export default function NotificationsTab() {
     });
   };
   return (
-    <>
-      <Card className="!h-auto flex-col">
-        <Row gutter={[16, 16]}>
-          <Col span={24} lg={8}>
-            <Typography.Title level={4}>Activity</Typography.Title>
-            <Typography.Text color={colorTextSecondary} className="opacity-70">
-              Donec mi odio, faucibus at, scelerisque quis
-            </Typography.Text>
-          </Col>
-          <Col span={24} lg={16}>
-            <div
-              className="flex w-full flex-col gap-4 rounded-lg px-6 py-8"
-              style={{ backgroundColor: colorBgContainerDisabled }}
-            >
-              <div className="flex w-full justify-between">
-                <div>Email me when someone answers on my form</div>
-                <Switch defaultChecked />
-              </div>
-              <div className="flex w-full justify-between">
-                <div>Email me when someone comments onmy article</div>
-                <Switch />
-              </div>
-              <div className="flex w-full justify-between">
-                <div>Email me hen someone follows me</div>
-                <Switch defaultChecked />
-              </div>
+    <Card className="!h-auto flex-col">
+      <Row gutter={[16, 16]}>
+        <Col span={24} lg={8}>
+          <Typography.Title level={4}>Activity</Typography.Title>
+          <Typography.Text color={colorTextSecondary} className="opacity-70">
+            Donec mi odio, faucibus at, scelerisque quis
+          </Typography.Text>
+        </Col>
+        <Col span={24} lg={16}>
+          <div
+            className="flex w-full flex-col gap-4 rounded-lg px-6 py-8"
+            style={{ backgroundColor: colorBgContainerDisabled }}
+          >
+            <div className="flex w-full justify-between">
+              <div>Email me when someone answers on my form</div>
+              <Switch defaultChecked />
             </div>
-          </Col>
-
-          <Col span={24} lg={8}>
-            <Typography.Title level={4}>Applications</Typography.Title>
-            <Typography.Text color={colorTextSecondary} className="opacity-70">
-              Donec mi odio, faucibus at, scelerisque quis
-            </Typography.Text>
-          </Col>
-          <Col span={24} lg={16}>
-            <div
-              className="flex w-full flex-col gap-4 rounded-lg px-6 py-8"
-              style={{ backgroundColor: colorBgContainerDisabled }}
-            >
-              <div className="flex w-full justify-between">
-                <div>News and announcements</div>
-                <Switch />
-              </div>
-              <div className="flex w-full justify-between">
-                <div>Weekly product updates</div>
-                <Switch defaultChecked />
-              </div>
-              <div className="flex w-full justify-between">
-                <div>Weekly blog digest</div>
-                <Switch />
-              </div>
+            <div className="flex w-full justify-between">
+              <div>Email me when someone comments onmy article</div>
+              <Switch />
             </div>
-          </Col>
-
-          <div className="flex w-full justify-end">
-            <Button type="primary" onClick={handleClick}>
-              Save Changes
-            </Button>
+            <div className="flex w-full justify-between">
+              <div>Email me hen someone follows me</div>
+              <Switch defaultChecked />
+            </div>
           </div>
-        </Row>
-      </Card>
-      <SkillAssessmentResult />
-    </>
+        </Col>
+
+        <Col span={24} lg={8}>
+          <Typography.Title level={4}>Applications</Typography.Title>
+          <Typography.Text color={colorTextSecondary} className="opacity-70">
+            Donec mi odio, faucibus at, scelerisque quis
+          </Typography.Text>
+        </Col>
+        <Col span={24} lg={16}>
+          <div
+            className="flex w-full flex-col gap-4 rounded-lg px-6 py-8"
+            style={{ backgroundColor: colorBgContainerDisabled }}
+          >
+            <div className="flex w-full justify-between">
+              <div>News and announcements</div>
+              <Switch />
+            </div>
+            <div className="flex w-full justify-between">
+              <div>Weekly product updates</div>
+              <Switch defaultChecked />
+            </div>
+            <div className="flex w-full justify-between">
+              <div>Weekly blog digest</div>
+              <Switch />
+            </div>
+          </div>
+        </Col>
+
+        <div className="flex w-full justify-end">
+          <Button type="primary" onClick={handleClick}>
+            Save Changes
+          </Button>
+        </div>
+      </Row>
+    </Card>
   );
 }
